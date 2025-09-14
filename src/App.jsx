3e27f3e7d196b6300b05bbc285babc30034bf6fe
Abhishek Ranjan <abhisheksingh70224@gmail.com>
2025-09-14 16:52:02 +0530

    Create pages and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import SignOut from "./pages/SignOut";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 
 export default function App() {
-  return <h1 className="text-green-500 text-5xl font-bold">App</h1>;
-  
+  return (<BrowserRouter>
+    <Routes>
+
+      <Route path='/' element={<Home />} />
+      <Route path='/sign-in' element={<Signin />} />
+      <Route path='/sign-out' element={<SignOut />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/profile' element={<Profile />} />
+
+    </Routes>
+  </BrowserRouter>
+  ) 
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Home() {
+  return <h1 style={{ color: 'red', fontSize: '30px' }}>🏠 Home Page is Working!</h1>;
+}
