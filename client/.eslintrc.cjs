c927c1e531dafe09148e4b1df5985fb7542181b1
Abhishek Ranjan <abhisheksingh70224@gmail.com>
2025-10-11 03:19:55 +0530

    commit three

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/prop-types': 'off',
+    'no-unused-vars': 'warn',
+    'react/react-in-jsx-scope': 'off',
+  },
+};
